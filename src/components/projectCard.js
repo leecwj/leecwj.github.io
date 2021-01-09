166399d518f782a1477dbf0a8bd677c6eba94b44
leecwj <37468566+leecwj@users.noreply.github.com>
2021-01-09 17:27:36 +1000

    link functionality (to repo/live) added to project cards

--- src/components/projectCard.js
+++ src/components/projectCard.js
@@ -1,6 +1,8 @@
 import React, {Component} from "react";
 import "./card.css";
 import "./projectCard.css";
+import gh from "../images/gh.svg";
+import link from "../images/link.svg";
 
 class ProjectCard extends Component {
 
@@ -17,11 +19,39 @@ class ProjectCard extends Component {
                     <img className="projectcard-img" src={this.props.imgSrc} alt={this.props.imgAlt}/>
                 </div>
                 <div className="card-desc">
-                    <div className="card-title"><h1>{this.props.cardTitle}</h1></div>
+                    <div className="card-title">
+                        <h1>
+                            {this.props.cardTitle} | {this.getGh()} {this.getLink()}
+                        </h1>
+                    </div>
                     {this.props.children}
                 </div>
             </div>
         );
+    }
+
+    getGh() {
+        if (this.props.gh) {
+            return (
+                <a href={this.props.gh} title="View project on GitHub">
+                    <img className="projectcard-gh" src={gh} alt="GitHub logo"/>
+                </a>
+            );
+        } else {
+            return null;
+        }
+    }
+
+    getLink() {
+        if (this.props.link) {
+            return (
+                <a href={this.props.link} title="View live demo">
+                    <img className="projectcard-link" src={link} alt="Project demo"/>
+                </a>
+            );
+        } else {
+            return null;
+        }
     }
 }
 
