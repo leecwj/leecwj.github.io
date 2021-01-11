a7637069d1adbe226ef4b27deaa01c263183fdd9
leecwj <37468566+leecwj@users.noreply.github.com>
2021-01-11 16:18:18 +1000

    navigation bar, routing added to app template

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import './App.css';
+import {HashRouter, Route, Switch} from "react-router-dom";
+import About from "./about";
+import Projects from "./projects";
+import selfSimple from "./images/self-simple.min.svg"
+import NavigationBar from "./components/navigationBar";
+import NavigationBarItem from "./components/navigationBarItem";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-
-      </header>
-    </div>
-  );
+    return (
+        <HashRouter basename="/">
+            <div className="App">
+                <nav className="navigation">
+                    <NavigationBar imgSrc={selfSimple} imgAlt="Logo">
+                        <NavigationBarItem to="/about" name="about">About</NavigationBarItem>
+                        <NavigationBarItem to="/projects" name="projects">Projects</NavigationBarItem>
+                    </NavigationBar>
+                </nav>
+                <Switch>
+                    <Route path="/about" component={About}/>
+                    <Route path="/projects" component={Projects}/>
+                </Switch>
+            </div>
+        </HashRouter>
+    );
 }
 
 export default App;
